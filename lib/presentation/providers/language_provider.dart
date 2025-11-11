bcf68fe86c305ff8bd0cbddaa4dc2c16376f8da5
mouna-kousri <mouna.kousri@bizerte.r-iset.tn>
2025-11-12 00:34:00 +0100

    opdate

--- lib/presentation/providers/language_provider.dart
+++ lib/presentation/providers/language_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 
 /// Provider pour gérer la langue de l'application
 final languageProvider = StateProvider<Locale>((ref) {
--- lib/presentation/providers/sensor_provider.dart
+++ lib/presentation/providers/sensor_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 import '../../models/sensor_data.dart';
 
 class SensorState {
